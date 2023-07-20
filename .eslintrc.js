443f67e23f93493cbd3ead789be24a9dcd204136
GabrielBdeC <gabrielbdec@gmail.com>
2023-07-19 21:31:06 -0300

    build: configuration and scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', "prettier", "no-loops"],
   extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -17,9 +19,6 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "no-loops/no-loops": "error"
   },
 };
